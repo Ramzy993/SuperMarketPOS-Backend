761cbf7c8ace5baa87aac098f45fde60c616d788
youssef ramzy <youssef.ramzy993@gmail.com>
2020-11-27 18:58:30 +0200

    create log manager

--- launch_app.py
+++ launch_app.py
@@ -2,10 +2,14 @@
 
 # lib imports
 
-
 # project imports
 from web_app.app import start_app
+from logger_manager.logger_manager import LogManger
+
+
+logger = LogManger().get_logger(__name__)
 
 
 if __name__ == '__main__':
+    logger.info("Welcome to SuperPos system ....")
     start_app()
--- web_app/app.py
+++ web_app/app.py
@@ -5,16 +5,21 @@ from flask import Flask
 
 # project imports
 from config_manager.config_manager import ConfigManager
+from logger_manager.logger_manager import LogManger
 from persistance_db_manager.db_driver import DBDriver
 
 
 app_config = ConfigManager().app_config
+logger = LogManger().get_logger(__name__)
+
 db_driver = DBDriver()
 
 
 def creat_app():
 
     app = Flask(import_name=__name__)
+
+    logger.info("Flask App created.")
 
     return app
 
@@ -27,5 +32,6 @@ def start_app(app=None):
     if app is None:
         app = creat_app()
 
+    logger.info(f"App is running on host name: {host}, port: {port}, with debug mode: {debug}")
     app.run(host=host, port=port, debug=debug, use_reloader=False)
 
